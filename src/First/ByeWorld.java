acfcc6cbbbac6aac7866e00d5526e9b2d48ca641
ovsanna <ovsanna@lionsinserance.com>
2022-08-23 08:31:20 -0700

    Added ByeWorld.java

---
+++ src/First/ByeWorld.java
@@ -0,0 +1,8 @@
+package First;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Maybe 5 weeks of Java");
+        System.out.println("1.5 of git ");
+    }
+}
